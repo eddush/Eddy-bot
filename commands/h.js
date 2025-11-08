d5e39fec3fd0b5bd9b9ec52f9d2107ac3b4010ee
eddush <eddyshermant@gmail.com>
2025-11-08 19:32:02 +0200

    Add help command with team role notification

--- commands/h.js
+++ commands/h.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+    name: "h",
+    description: "Help command for team",
+    execute(message, args) {
+        if (args[0] === "test") {
+            // מוצא את הרול וצובע אותו
+            const teamRole = message.guild.roles.cache.find(role => role.name === "צוות");
+            if (teamRole) {
+                message.channel.send(`${teamRole} צוות, יש קריאה לעזרה! 🆘`);
+            } else {
+                message.channel.send("לא נמצא רול בשם 'צוות'");
+            }
+        } else {
+            message.channel.send("שימוש לא נכון. נסה !h test");
+        }
+    }
+};
